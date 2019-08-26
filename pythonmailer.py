1c7c515b2cf66beb608ec275973b8a09eb28ad8b
GraniteBeltWeb <40533161+GraniteBeltWebAndSoftwareSolutions@users.noreply.github.com>
2019-08-27 01:20:49 +1000

    Add files via upload

---
+++ pythonmailer.py
@@ -0,0 +1,20 @@
+import smtplib
+
+
+fromaddr = 'xxxx'
+toaddr = 'xxxxx'
+user = 'xxxxxx'
+pas = 'xxxxxx'
+
+def sendmail():
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(user, pas)
+    server.sendmail(fromaddr, toaddr, msg)
+    server.quit()
+
+
+msg = raw_input('Enter  a message to send please.... ')
+
+  sendmail()
+print (msg)
